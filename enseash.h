e04ce3cec36cfbf276c60bdbf49b9fb0a4c75f57
K. M-Larafale <73236841+blblbl-skiski@users.noreply.github.com>
2021-12-19 18:17:34 +0100

    Update enseash.h

--- enseash.h
+++ enseash.h
@@ -1,8 +1,23 @@
 #define COMMAND_SIZE 50
 #define PRINT_SIZE 80
 #define PATH_SIZE 150
+#define StartMsg "Welcome to ENSEA Tiny Shell \nPour quitter, tapez 'exit'. \nenseash%"
 
 
 int main(int argc, char **argv);
+
+void ChildState(int state,long exectime);
+
+char** FormatArgs(char *args);
+
+int DetectChar(char **args,char * find);
+
+int SpaceCount(char *);
+
+void Display(char *);
+
+char** ResToFile(char **args);
+
+
 
 
